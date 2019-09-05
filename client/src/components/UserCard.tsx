9334902f3aab4eaae26952ef6ce1d09af285c318
Philippe SIM <psim@e1r10p20.42.fr>
2019-09-05 19:18:14 +0200

    Fixed some css

---
+++ client/src/components/UserCard.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import "../styles/stylesUserHome.css";
+
+class UserCard extends React.Component {
+  public render() {
+    return (
+      <div className="ui card user">
+        <div className="image">
+          <img
+            className="profile-pic"
+            src="http://localhost:5000/public/profile-pictures/tracteur.jpg"
+          />
+        </div>
+        <div className="content">
+          <a className="header">psim</a>
+          <div className="meta">
+            <span className="date">Offline since 1 month</span>
+          </div>
+          <div className="description">I liek eggs XD</div>
+        </div>
+        <div className="extra content">
+          <a>
+            <i className="road icon"></i>
+            42 km
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+export default UserCard;
