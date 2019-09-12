66d5b96d018571a1afa3a6a00f1fbd24de8a5347
Philippe SIM <psim@e1r2p7.42.fr>
2019-09-12 15:04:13 +0200

    Now rendering component UserCard for each matched users

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -1,28 +1,59 @@
 import * as React from "react";
 import "../styles/stylesUserHome.css";
+import Pictures from "./Pictures";
 
-class UserCard extends React.Component {
+interface Props {
+  userInfo: {
+    id: string;
+    name: string;
+    city: string;
+    age: string;
+    connection: string;
+    pictures: [
+      {
+        path: string;
+        date: string;
+        main: boolean;
+      }
+    ];
+  };
+}
+
+class UserCard extends React.Component<Props> {
   public render() {
+    var today = new Date();
+    var lastSeen = new Date(this.props.userInfo.connection);
+    var msInDay = 24 * 60 * 60 * 1000;
+
+    lastSeen.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
+
+    var lastSeenSince = (+today - +lastSeen) / msInDay;
+    // console.log(lastSeenSince);
+    var userPic = this.props.userInfo.pictures;
+    var userPicMain = userPic[0];
+    console.log(userPicMain.path);
     return (
       <div className="ui card user">
         <div className="image">
           <img
             alt="profile-pic"
-            className="profile-pic"
-            src="http://localhost:5000/public/profile-pictures/tracteur.jpg"
+            className="profile-pic-card"
+            // src={`http://localhost:5000/public/profile-pictures/${userPicMain.path}`}
+            src="http://localhost:5000/public/profile-pictures/psim.jpg"
           />
         </div>
         <div className="content">
-          <a className="header">psim</a>
+          <a className="header">{this.props.userInfo.name}</a>
           <div className="meta">
-            <span className="date">Offline since 1 month</span>
+            <span className="date">Connected {lastSeenSince} days ago</span>
           </div>
-          <div className="description">I liek eggs XD</div>
+          <div className="description">blblbl</div>
         </div>
         <div className="extra content">
           <a>
             <i className="road icon"></i>
-            42 km
+            {this.props.userInfo.city}
           </a>
         </div>
       </div>
