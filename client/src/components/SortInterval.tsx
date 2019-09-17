1676bf096b71abbf8dc8ba319e0d796403c95240
Jennifer CHARLOIS <jcharloi@e1r3p9.42.fr>
2019-09-17 12:49:23 +0200

    Displayed values of interval

--- client/src/components/SortInterval.tsx
+++ client/src/components/SortInterval.tsx
@@ -17,6 +17,12 @@ interface Props {
 }
 
 interface State {
+  startAge: number;
+  endAge: number;
+  startLoc: number;
+  endLoc: number;
+  startPop: number;
+  endPop: number;
   tags: Array<Tags>;
   list: Array<string>;
   messageTags?: string;
@@ -26,6 +32,12 @@ class SortInterval extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
+      startAge: 50,
+      endAge: 70,
+      startLoc: 130,
+      endLoc: 180,
+      startPop: 40,
+      endPop: 64,
       list: [],
       tags: []
     };
@@ -60,26 +72,36 @@ class SortInterval extends React.Component<Props, State> {
       <div>
         <div className="container-range">
           Sort by age :
+          <div>
+            <span>{this.state.startAge}</span>
+            <span>{this.state.endAge}</span>
+          </div>
           <div className="range">
             <span>18</span>
             <Range
               min={18}
               max={100}
-              defaultValue={[25, 40]}
+              defaultValue={[50, 70]}
               onChange={value => {
+                this.setState({ startAge: value[0], endAge: value[1] });
                 this.props.sortByInterval("Age", value[0], value[1], []);
               }}
             />
             <span>100 yo</span>
           </div>
           Sort by location :
+          <div>
+            <span>{this.state.startLoc}</span>
+            <span>{this.state.endLoc}</span>
+          </div>
           <div className="range">
             <span>0</span>
             <Range
               min={0}
               max={300}
-              defaultValue={[25, 40]}
+              defaultValue={[130, 180]}
               onChange={value => {
+                this.setState({ startLoc: value[0], endLoc: value[1] });
                 this.props.sortByInterval(
                   "Localisation",
                   value[0],
@@ -91,13 +113,18 @@ class SortInterval extends React.Component<Props, State> {
             <span>300 km</span>
           </div>
           Sort by popularity :
+          <div>
+            <span>{this.state.startPop}</span>
+            <span>{this.state.endPop}</span>
+          </div>
           <div className="range">
             <span>0</span>
             <Range
               min={0}
               max={100}
-              defaultValue={[25, 40]}
+              defaultValue={[40, 64]}
               onChange={value => {
+                this.setState({ startPop: value[0], endPop: value[1] });
                 this.props.sortByInterval("Popularity", value[0], value[1], []);
               }}
             />
