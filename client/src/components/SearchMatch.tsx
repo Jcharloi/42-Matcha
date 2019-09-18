5dbe79db5072397334c81032dcd57ec05b196886
Jennifer CHARLOIS <jcharloi@e1r3p9.42.fr>
2019-09-18 20:23:04 +0200

    Started search match page, need to fix bug where I don't parse correctly my preference/index

--- client/src/components/SearchMatch.tsx
+++ client/src/components/SearchMatch.tsx
@@ -1,18 +1,135 @@
 import * as React from "react";
-import TopMenu from "./TopMenu";
+import Axios from "axios";
+import { State } from "../redux/types/types";
+import { UserMatchInfos } from "../models/models";
+
+import TopMenu from "../components/TopMenu";
+import UserCard from "../components/UserCard";
+import SortIndex from "../components/SortIndex";
+import FilterInterval from "../components/FilterInterval";
 
 import "../styles/stylesUserHome.css";
+import { connect } from "react-redux";
 
-class SearchMatch extends React.Component<{}, {}> {
+interface Props {
+  user_id: string;
+  mail: string;
+  user_name: string;
+  last_name: string;
+  first_name: string;
+  birthday: string;
+  gender: string;
+  orientation: string;
+  presentation: string;
+  score: string;
+  city: string;
+  pictures: any;
+  tags: any;
+}
+
+interface HState {
+  isLoading: boolean;
+  clearList: boolean;
+  userMatchInfo: Array<UserMatchInfos>;
+  copyUserMatch: Array<UserMatchInfos>;
+  messageHome?: string;
+}
+
+class Search extends React.Component<Props, HState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      clearList: false,
+      userMatchInfo: [],
+      copyUserMatch: []
+    };
+  }
+
+  // sortByIndex = (indexBy: string) => {
+  //   const userAge =
+  //     new Date().getFullYear() - +this.props.birthday.split("/")[2];
+  //   Axios.post("http://localhost:5000/home/sort-by-index", {
+  //     userName: localStorage.getItem("user_name"),
+  //     token: localStorage.getItem("token"),
+  //     index: indexBy,
+  //     age: userAge.toString(),
+  //     userMatchInfo: this.state.userMatchInfo
+  //   })
+  //     .then(({ data: { validated, message, userMatchInfo } }) => {
+  //       if (validated) {
+  //         this.setState({ userMatchInfo });
+  //       }
+  //       this.setState({ messageHome: message, isLoading: false });
+  //     })
+  //     .catch(err => console.error(err));
+  // };
+
+  searchByInterval = (
+    index: string,
+    startAge: number,
+    endAge: number,
+    startLoc: number,
+    endLoc: number,
+    startPop: number,
+    endPop: number,
+    tagsName: Array<string>,
+    preference: string
+  ) => {
+    Axios.put("http://localhost:5000/search/get-users-by-search/", {
+      userName: localStorage.getItem("user_name"),
+      token: localStorage.getItem("token"),
+      index,
+      preference,
+      startAge: startAge.toString(),
+      endAge: endAge.toString(),
+      startLoc: startLoc.toString(),
+      endLoc: endLoc.toString(),
+      startPop: startPop.toString(),
+      endPop: endPop.toString(),
+      tagsName
+    })
+      .then(({ data: { validated, message, userMatchInfo } }) => {
+        if (validated) {
+          this.setState({ userMatchInfo });
+        }
+        this.setState({ messageHome: message, isLoading: false });
+      })
+      .catch(err => console.error(err));
+  };
+
+  clearMatch = () => {
+    this.setState({ userMatchInfo: this.state.copyUserMatch });
+  };
+
   public render() {
     return (
       <div>
-        <TopMenu current="search" />
-        <br />
-        cc
+        <TopMenu current="home" />
+        <div>
+          {/* <SortIndex sortByIndex={this.sortByIndex} /> */}
+          <FilterInterval
+            isSearch={true}
+            byInterval={this.searchByInterval}
+            clearMatch={this.clearMatch}
+          />
+        </div>
+        {!this.state.isLoading &&
+          this.state.userMatchInfo.map(user => (
+            <UserCard key={user.id} userInfo={user} />
+          ))}
+        {this.state.messageHome && (
+          <div className="toast-message ui blue floating message">
+            <p>{this.state.messageHome}</p>
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default SearchMatch;
+const mapStateToProps = (state: State): Props => {
+  return state.user;
+};
+
+export default connect(mapStateToProps)(Search);
