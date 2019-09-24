df5687c35b30c29f3f8023ec09df23207abb9ab3
Jennifer CHARLOIS <jcharloi@e1r3p9.42.fr>
2019-09-24 16:05:33 +0200

    Added user match infos to redux store when clicking on his profile

--- client/src/redux/reducers/reducers.ts
+++ client/src/redux/reducers/reducers.ts
@@ -1,12 +1,12 @@
 import {
   InsertProfileAction,
   INSERT_USER_PROFILE,
-  UpdateAuthAction,
   UPDATE_USER_AUTH,
   INSERT_OTHER_PROFILE,
+  UpdateAuthAction,
   InsertOtherProfileAction
 } from "../types/types";
-import { User, VerifiedUser } from "../../models/models";
+import { VerifiedUser, UserMatchInfos } from "../../models/models";
 
 let initialState = {
   user_id: "",
@@ -21,23 +21,43 @@ let initialState = {
   score: "",
   city: "",
   pictures: [{ path: "", date: "", main: false }],
-  tags: [{ tag_id: "", name: "", custom: false }],
-  liked: false
+  tags: [{ tag_id: "", name: "", custom: false }]
 };
 
 export const userProfileReducer = (
   state = initialState,
-  action: InsertProfileAction | InsertOtherProfileAction
-): User => {
+  action: InsertProfileAction
+) => {
   switch (action.type) {
     case INSERT_USER_PROFILE: {
       return Object.assign({}, state, action.payload);
     }
-    case INSERT_OTHER_PROFILE: {
-      return Object.assign({}, state, action.payload);
-    }
     default:
       return state;
+  }
+};
+
+let initialStateOther = {
+  id: "",
+  name: "",
+  city: "",
+  age: "",
+  connection: "",
+  gender: "",
+  popularityScore: "",
+  pictures: [{ path: "", date: "", main: false }],
+  tags: [{ tag_id: "", name: "", custom: false }],
+  liked: false
+};
+
+export const otherProfileReducer = (
+  state = initialStateOther,
+  action: InsertOtherProfileAction
+): UserMatchInfos => {
+  if (action.type === INSERT_OTHER_PROFILE) {
+    return Object.assign({}, state, action.payload);
+  } else {
+    return state;
   }
 };
 
