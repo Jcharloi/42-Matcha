3b83554a9a7ebfd65f9622e12e4bab7f9a73a0d2
Philippe SIM <psim@e1r4p11.42.fr>
2019-10-15 19:47:51 +0200

    deleted double socket connection

--- client/src/helpers/socket.js
+++ client/src/helpers/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
 const socket = socketIOClient("http://localhost:5001/");
-socket.emit("send-userName", localStorage.getItem("user_name"));
+socket.emit("send-user-name", localStorage.getItem("user_name"));
 
 export default socket;
